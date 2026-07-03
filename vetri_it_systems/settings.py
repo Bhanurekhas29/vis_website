14d92dab4f5dfbd9ac8f6c3a230e2b01bb94fac4
Bhanurekhas29 <prettythingsbyb.2023@gmail.com>
2026-07-03 09:58:32 +0530

    Add environment-based DB config for PythonAnywhere deployment

--- vetri_it_systems/settings.py
+++ vetri_it_systems/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,19 +76,28 @@ WSGI_APPLICATION = 'vetri_it_systems.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "vetri_it_systems_db",
-        "USER": "root",
-        "PASSWORD": "MySQL@2026",
-        "HOST": "localhost",
-        "PORT": "3306",
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+if os.environ.get('PYTHONANYWHERE_SITE'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": "vetri_it_systems_db",
+            "USER": "root",
+            "PASSWORD": "MySQL@2026",
+            "HOST": "localhost",
+            "PORT": "3306",
+            "OPTIONS": {
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
+    }
+
 
 
 # Password validation
