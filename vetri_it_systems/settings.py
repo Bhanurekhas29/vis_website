ab16cb48c9e74edd12410627cd29e8aa704f36a7
Bhanurekhas29 <prettythingsbyb.2023@gmail.com>
2026-07-03 10:37:25 +0530

    Fix STATICFILES_DIRS path to core/static

--- vetri_it_systems/settings.py
+++ vetri_it_systems/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
+
+
 
 
 MEDIA_URL = "/media/"
